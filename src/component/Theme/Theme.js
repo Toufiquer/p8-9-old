c71d42019341a87440936b0d6617bc9e43c1b29a
Toufiquer <toufiquerabc@gmail.com>
2023-01-22 01:56:37 +0600

    theme toggle complete

--- src/component/Theme/Theme.js
+++ src/component/Theme/Theme.js
@@ -1,51 +1,48 @@
 import React, { useContext, useState } from "react";
-import { CheckCircleIcon, ChevronDownIcon, MoonIcon, StarIcon, SunIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon, MoonIcon, StarIcon, SunIcon } from "@heroicons/react/24/solid";
 import { ThemeData, ThemeMode } from "../../App";
 import getThemeVariants from "./getThemeVariants";
 
 const Theme = () => {
   const [themeMode, setThemeMode] = useContext(ThemeMode);
   const [themeData, setThemeData] = useContext(ThemeData);
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(true); //   Get Next Theme data and set it to themeData
+  const SetNewTheme = (themeName) => {
+    const themeData = getThemeVariants(themeName);
+    setThemeData(themeData);
+    setThemeMode(themeName);
+  };
   const handleTheme = () => {
-    //   Get Next Theme data and set it to themeData
-    const setData = (themeName) => {
-      const themeData = getThemeVariants(themeName);
-      setThemeData(themeData);
-    };
     //   Set Theme Variant.
     //  Theme Value or custom css in getThemeVariants.js
     switch (themeMode) {
       case "dark":
-        setData("light");
-        setThemeMode("light");
+        SetNewTheme("light");
+        console.log(themeMode, "inside");
         break;
       case "light":
-        setData("sky");
-        setThemeMode("sky");
+        SetNewTheme("sky");
         break;
       case "sky":
-        setData("dark");
-        setThemeMode("dark");
+        SetNewTheme("dark");
         break;
       default:
-        setThemeMode(themeMode);
+      // setThemeMode(themeMode);
     }
   };
-  const { card, backgroundII, text, hoverTextII } = themeData;
-  console.log(card, themeMode);
+  const { backgroundII, text, hoverTextII } = themeData;
   const iconClass = ` h-6 w-6 ${text} cursor-pointer`;
   return (
     <div className={`flex gap-2`}>
       <div className="" onClick={handleTheme}>
-        {(themeMode === "dark" && <SunIcon className={`${iconClass}`} />) || (themeMode === "light" && <StarIcon className={`${iconClass}`} />) || (themeMode === "sky" && <MoonIcon className={`${iconClass}`} />)}
+        {(themeMode === "dark" && <SunIcon className={`${iconClass}`} />) || (themeMode === "light" && <StarIcon className={`${iconClass}`} />) || (themeMode === "sky" && <MoonIcon className={`${iconClass}`} />) || "Theme"}
       </div>
       <div>
         <ChevronDownIcon className={`${iconClass}`} onClick={() => setToggle(!toggle)} />
         <div className={`${toggle ? "hidden" : "block"} absolute ${backgroundII} py-2 px-1 ml-[-5px]`}>
-          <SunIcon className={`${iconClass} ${hoverTextII} m-1`} />
-          <MoonIcon className={`${iconClass} ${hoverTextII} m-1`} />
-          <StarIcon className={`${iconClass} ${hoverTextII} m-1`} />
+          <SunIcon onClick={() => SetNewTheme("light")} className={`${iconClass} ${hoverTextII} m-1`} />
+          <MoonIcon onClick={() => SetNewTheme("dark")} className={`${iconClass} ${hoverTextII} m-1`} />
+          <StarIcon onClick={() => SetNewTheme("sky")} className={`${iconClass} ${hoverTextII} m-1`} />
         </div>
       </div>
     </div>
