dced5361941ce4311491c0e1dbc00cfa921efdae
Toufiquer <toufiquerabc@gmail.com>
2023-01-22 02:53:31 +0600

    toggle with nabvar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,17 +1,25 @@
-import React, { useContext } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import React, { useContext, useState } from "react";
 import { ThemeData } from "../../App";
 import Theme from "../Theme/Theme";
 import CustomLink from "./CustomLink";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
   const [themeData] = useContext(ThemeData);
-  const { backgroundI, hoverBackgroundI, text, hoverText, textI, hoverTextI } = themeData;
+  const { backgroundI, hoverBackgroundI, text, hoverText, textI, hoverTextI, textII, hoverTextII } = themeData;
+  const iconClass = ` h-6 w-6 ${textII} ${hoverTextII} cursor-pointer`;
   return (
-    <div className={`pb-3 pt-2 ${backgroundI} ${text} text-xl ${hoverBackgroundI}`}>
-      <header className={`flex justify-between items-center container mx-auto`}>
-        <span className={`flex justify-between items-center ${textI} ${hoverTextI} text-semibold`}>Toufiquer</span>
-        <nav className={`flex gap-4`}>
-          <ul className={`flex gap-4`}>
+    <div className={`pb-3 pt-2 ${backgroundI} ${text} text-xl ${hoverBackgroundI} text-center  duration-1000`}>
+      <header className={`flex justify-between items-center container mx-auto flex-col md:flex-row`}>
+        <h2 className={`flex justify-between items-center ${textI} ${hoverTextI} text-semibold text-left md:w-auto w-full px-4`}>
+          Toufiquer{" "}
+          <span className={`md:hidden block`} onClick={() => setToggle(!toggle)}>
+            {toggle ? <Bars3Icon className={`${iconClass}`}></Bars3Icon> : <XMarkIcon className={`${iconClass}`}></XMarkIcon>}
+          </span>
+        </h2>
+        <nav className={`flex gap-4 flex-col md:flex-row mt-2 md:mt-0 ${!toggle ? "block" : "hidden"} md:flex`}>
+          <ul className={`flex gap-4 flex-col md:flex-row `}>
             <li>
               <CustomLink hoverText={hoverText} to="/">
                 Home
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App className={`duration-800 `} />
     </BrowserRouter>
     ,
   </React.StrictMode>
