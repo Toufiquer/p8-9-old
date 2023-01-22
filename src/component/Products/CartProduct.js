4453e075e4d8069cb18f8f493b635c52aeaed115
Toufiquer <toufiquerabc@gmail.com>
2023-01-23 00:16:15 +0600

    products design and products carts design complere

--- src/component/Products/CartProduct.js
+++ src/component/Products/CartProduct.js
@@ -1,7 +1,24 @@
+import { TrashIcon } from "@heroicons/react/24/solid";
 import React from "react";
+import { useContext } from "react";
+import { ThemeData } from "../../App";
 
-const CartProduct = ({ products, handleRemove, handleAdd }) => {
-  return <div>Cart Product</div>;
+const CartProduct = ({ product: { id, picture, name }, handleRemove }) => {
+  const [themeData] = useContext(ThemeData);
+  const { border } = themeData;
+  return (
+    <div className={`${border} m-1 p-1`}>
+      <div className="flex justify-between w-full">
+        <div className="flex">
+          <img className={`h-12 w-12 object-cover mr-1`} src={picture} alt={name} />
+          <h2 title={name}>{name.length >= 20 ? name.slice(0, 20) + "..." : name}</h2>
+        </div>
+        <div className="flex items-center justify-center">
+          <TrashIcon onClick={() => handleRemove(id)} className={`h-6 w-6 ml-[-5px] text-red-500 hover:text-red-600 cursor-pointer`}></TrashIcon>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartProduct;
--- src/component/Products/ProductsCart.js
+++ src/component/Products/ProductsCart.js
@@ -6,8 +6,8 @@ const ProductsCart = ({ products, handleRemove, handleAdd }) => {
   const [themeData] = useContext(ThemeData);
   const { border, background } = themeData;
   const maxProducts = [...products];
-  if (maxProducts.length >= 4) {
-    maxProducts.length = 4;
+  if (maxProducts.length >= 8) {
+    maxProducts.length = 8;
   }
   return (
     <div className={`${border} p-2 text-center text-2xl ${background} z-10`}>
@@ -16,7 +16,7 @@ const ProductsCart = ({ products, handleRemove, handleAdd }) => {
       {maxProducts.map((curr) => (
         <CartProduct key={curr.id} product={curr} handleRemove={handleRemove} handleAdd={handleAdd}></CartProduct>
       ))}
-      {products.length >= 5 && <h2 className={`${border} p-1 m-1`}>You Have {products.length - 4} More Products</h2>}
+      {products.length >= 8 + 1 && <h2 className={`${border} p-1 m-1`}>You Have {products.length - 8} More Products</h2>}
     </div>
   );
 };
